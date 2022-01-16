e990aa90fc9fa532a9e649cb41be86f9e5b92358
handeerdal <erdalhande@hotmail.com>
2022-01-16 19:09:55 +0300

    Palindrom Sayı

---
+++ Palindrom Sayı.c
@@ -0,0 +1,32 @@
+
+//  Girilen Sayının Palindrom Sayı Olup Olmadığını Bulma
+
+//  Created by Hande Erdal on 8.12.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int temp,kalan,sayi,toplam=0;
+    printf("Palindrom sayı olup olmadığını öğrenmek istedğiniz sayıyı girin");
+    scanf("%d",&sayi);
+    temp=sayi;
+    while(temp>0)
+    {
+        kalan=temp%10;
+        toplam=kalan+(toplam*10);
+        temp/=10;
+    }
+    if(toplam==sayi)
+    {
+        printf("Evet bu bir palindrom sayı");
+    }
+    else
+    {
+        printf("Palindrom olsa söylerdim knk");
+    }
+    
+    
+    
+    return 0;
+}
